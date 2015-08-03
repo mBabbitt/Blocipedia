a6f7e4d6fb6ea81905d81f6652e4090745be0d1c
mBabbitt <meganbabbitt23@gmail.com>
2015-08-03 08:37:15 -0700

    adding wikis

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: 'welcome#index'
-  #get 'welcome/index'
+  
+  root 'welcome#index'
+
+  resources :wikis
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
