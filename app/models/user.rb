e91f5dc167d9fdcb67fbfbd873079dbc9320c363
mBabbitt <meganbabbitt23@gmail.com>
2015-08-15 11:21:47 -0700

    collaborators almost done

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
  has_many :wikis, dependent: :destroy
+ 
  has_many :collaborators, through: :wikis
 
 
